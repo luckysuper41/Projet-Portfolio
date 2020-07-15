cdf9ee42b44a2a96f30481651a7c73a0a3b4b5c3
NGUYEN Huynh Dung <57275058+luckysuper41@users.noreply.github.com>
2020-07-15 22:24:49 +0200

    xong commit

--- js/script.js
+++ js/script.js
@@ -1,36 +1,6 @@
-/*Header*/
-/*
-$(document).ready(function() {
-	$(window).scroll(function(event) {
-		var pos_body = $('html,body').scrollTop();
-     
-	// console.log(pos_body);
-	if(pos_body>60){
-      $('.header-nav').addClass('scroll-header');
-      $('.nav-link').addClass('scroll-item');
-      $('header a.logo span').addClass('scroll-logo');
-      $('header #toggle').addClass('scroll-toggle');
-      $('.wrapper-header .sidebar').addClass('scroll-wrapper');
-      }
-	else {
-      $('.header-nav').removeClass('scroll-header');
-      $('.nav-link').removeClass('scroll-item');
-      $('header a.logo span').removeClass('scroll-logo');
-      $('header #toggle').removeClass('scroll-toggle');
-      $('.wrapper-header .sidebar').removeClass('scroll-wrapper');
-	}
-     });
-       
-     $('.back-to-top').click(function(event) {
-          $('html,body').animate({
-               scrollTop: 0},
-               1400);
-	});
-
-});*/
 
 /* About */
-const buttons_eau = document.querySelectorAll('.hobbies-item');
+const buttons_eau = document.querySelectorAll('.hobbies-item, #projets-item');
 buttons_eau.forEach(btn => {
      btn.addEventListener('mouseenter',function(){         
           let ripples = document.createElement('main');
@@ -40,14 +10,14 @@ buttons_eau.forEach(btn => {
 
           setTimeout(() =>{
                ripples.remove()
-          }, 1000);
+          }, 800);
      })
 });
 
-     
+/*Scroll*/
 $(document).ready(function() {
 
-     $('#navigation a,.sidebar a,.loader a').click(function(event) {
+     $('#navigation a,.sidebar a,.loader a,.scroll-top a,.header-nav a').click(function(event) {
      event.preventDefault();
      
      part = $(this).attr('href');
@@ -58,3 +28,51 @@ $(document).ready(function() {
      $('html, body').animate({scrollTop: position},1400);
      });
 });
+
+
+/*Contact*/
+const followButton = document.getElementById("Follow");
+const feedBackButton = document.getElementById("feedBack");
+const containerrr = document.getElementById("contact-content");
+
+followButton.addEventListener("click", () => {
+  containerrr.classList.add('right-panel-active');
+});
+
+feedBackButton.addEventListener("click", () => {
+  containerrr.classList.remove('right-panel-active');
+});
+
+/*backtotop*/
+window.addEventListener('scroll', function(){
+     var scroll = document.querySelector('.scroll-top');
+     scroll.classList.toggle('active', window.scrollY > 2000)
+})
+
+
+/*Start-header*/
+function stars(){
+
+     let count = 50;
+     let scene = document.querySelector('.scene');
+     let i=0;
+   
+     while(i < count){
+       let start = document.createElement('i');
+       let x = Math.floor(Math.random() * window.innerWidth);
+   
+       let duration = Math.random() * 1;
+       let h = Math.random() * 100;
+   
+       start.style.left = x +'px';
+       start.style.width = 1 +'px';
+       start.style.height = 2*h +'px';
+       start.style.animationDuration = duration + 's';
+   
+       scene.appendChild(start);
+       i++;
+     }
+   
+   }
+   
+   stars();
